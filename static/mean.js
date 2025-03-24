bdb55e41a17ee7dae4e497ecb804f919a3a408e7
AzizMardonqulov <=>
2025-03-25 00:05:45 +0500

    add loader part in home page style and responsive style 🗡️

--- static/mean.js
+++ static/mean.js
@@ -1,9 +1,13 @@
 let medioNavbar = document.getElementById("medioNavbar");
 let bars = document.getElementById("bars");
 let close =document.getElementById("close");
+let loader = document.getElementById("loader")
 bars.addEventListener("click" , function(){
     medioNavbar.style.top="0";
 })
 close.addEventListener("click" , function(){
     medioNavbar.style.top="-150vh";
+})
+window.addEventListener("load" , function(){
+    loader.classList.add("loder")
 })
